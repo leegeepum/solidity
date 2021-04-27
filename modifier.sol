3cfb17a2c247db7666d42a52161ab29784a0327a
leegeepum <77970942+leegeepum@users.noreply.github.com>
2021-04-27 11:15:13 +0800

    Create modifier.sol

---
+++ modifier.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.7.5;
+
+contract HelloWorld {
+    
+    mapping(address => uint) balance;
+    address owner;
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _; // run the function
+    }
+    
+    constructor(){
+        owner = msg.sender;
+    }
+    
+    function addBalance(uint _toAdd) public onlyOwner returns (uint) {
+        balance[msg.sender] += _toAdd;
+        return balance[msg.sender];
+    }
+    
+    function getBalance() public view returns (uint) {
+        return balance[msg.sender];
+    }
+    
+    function transfer(address recipient, uint amount) public {
+        require(balance[msg.sender] >= amount, "Balance not sufficient");
+        require(msg.sender != recipient, "Don't transfer money to yourself");
+        
+        uint previousSenderBalance = balance[msg.sender];
+        
+        _transfer(msg.sender, recipient, amount);
+        
+        assert(balance[msg.sender] == previousSenderBalance - amount);
+    }
+    
+    function _transfer(address from, address to, uint amount) private {
+        balance[from] -= amount;
+        balance[to] += amount;
+    }
+}
